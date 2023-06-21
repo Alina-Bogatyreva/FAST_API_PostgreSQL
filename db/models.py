c9c45c995b2d2a01da8173144b9d3d794241fc45
Alina-Bogatyreva <alina.bogatyreva2000@yandex.ru>
2023-06-21 19:53:51 +0300

    update project

--- db/models.py
+++ db/models.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Boolean, String, ARRAY, Integer
-# from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -9,7 +8,7 @@ Base = declarative_base()
 class Person(Base):
     __tablename__ = "person"
 
-    id = Column(Integer, unique=True, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, unique=True)
     firstname = Column(String(32), nullable=False)
     lastname = Column(String(32), nullable=False)
     spouse_id = Column(Integer, nullable=True)
@@ -17,12 +16,5 @@ class Person(Base):
     is_married = Column(Boolean, nullable=False)
 
     def __repr__(self):
-        return f"id: {self.id}, firstname: {self.firstname}, lastname: {self.lastname}"
-        # return 'id: {}, firstname: {}'.format(self.id, self.firstname)
-
-
-class Product(Base):
-    __tablename__ = "products"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(32), nullable=False)
+        return f"id: {self.id}, firstname: {self.firstname}, lastname: {self.lastname}, spouse_id: {self.spouse_id}," \
+               f"kids_ids: {self.kids_ids}, is_married: {self.is_married}\n"
--- main.py
+++ main.py
@@ -1,14 +1,49 @@
-from db.client import DBClientPerson
+from db import DBClientPerson
 from db.session import Session
-
 
 session = Session()
 client = DBClientPerson(session)
-print(client.get_person_by_id(1))
-client.add_person("Alina", "Bogatyreva", False, 2, [4, 9])
-print(client.get_person_by_id(1))
-print(client.get_persons())
-client.update_person(1, "ALINA", "BOGATYREVA", False, 2, [4, 9, 10])
-print(client.get_person_by_id(1))
-print(client.get_person_by_kids_ids([4, 9, 10]))
-print(client.delete_person(1))
+mikhail = client.add_person("Mikhail", "I", False)
+maria = client.add_person("Maria", "Dolgorukova", True, mikhail.id)
+pelagiya = client.add_person("Pelagiya", "", False)
+anna = client.add_person("Anna", "", False)
+marfa = client.add_person("Marfa", "", False)
+alexis = client.add_person("Alexis", "", False)
+mariaMiloslavskaya = client.add_person("Maria", "Miloslavskaya", True, alexis.id)
+сatherine = client.add_person("Catherine", "", False)
+feodosia = client.add_person("Feodosia", "", False)
+fedor = client.add_person("Fedor", "III", False)
+marfaApraksina = client.add_person("Maria", "Apraksina", True, fedor.id)
+ivan = client.add_person("Ivan", "V", False)
+sofia = client.add_person("Sofia", "", False)
+praskovia = client.add_person("Praskovia", "", True, ivan.id)
+peter = client.add_person("Peter", "I", False)
+feodosiaIvanovna = client.add_person("Feodosia", "", False)
+natalya = client.add_person("Natalya", "III", False)
+сatherineWife = client.add_person("Catherine", "", True, peter.id)
+alexander = client.add_person("Alexander", "V", False)
+pavel = client.add_person("Pavel", "", False)
+
+client.update_person(mikhail.id, kids_ids=[pelagiya.id, anna.id, marfa.id, alexis.id])
+client.update_person(maria.id, kids_ids=[pelagiya.id, anna.id, marfa.id, alexis.id])
+client.update_person(mariaMiloslavskaya.id, kids_ids=[сatherine.id, feodosia.id, fedor.id])
+client.update_person(alexis.id, kids_ids=[сatherine.id, feodosia.id, fedor.id])
+client.update_person(fedor.id, kids_ids=[ivan.id, sofia.id])
+client.update_person(marfaApraksina.id, kids_ids=[ivan.id, sofia.id])
+client.update_person(ivan.id, kids_ids=[peter.id, feodosiaIvanovna.id, natalya.id])
+client.update_person(praskovia.id, kids_ids=[peter.id, feodosiaIvanovna.id, natalya.id])
+client.update_person(peter.id, kids_ids=[alexander.id, pavel.id])
+client.update_person(сatherineWife.id, kids_ids=[alexander.id, pavel.id])
+
+
+
+
+
+"""
+                                       Mikhail I  +  Maria Dolgorukova
+                  1.Pelagiya  2.Anna  3.Marfa  4.Alexis + Maria Miloslavskaya
+                                        1.Catherine  2.Feodosia  3.Feodor III+Marfa Apraksina
+                                                                      1.Ivan V+Praskovia Saltykova  2.Sofia
+                                                                    1.Peter I+Catherine  2.Feodosia   3.Natalya
+                                                                1.Alexander  2.Pavel
+"""
